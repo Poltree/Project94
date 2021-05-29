be98f27861dc03543aca6460f22af98f4370570d
Poltree <76885489+Poltree@users.noreply.github.com>
2021-05-29 13:29:07 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyCKxK-_vYvE0ikNNavHNTwJfYciJ7Pg3B8",
+      authDomain: "lets-chat-ce40a.firebaseapp.com",
+      databaseURL: "https://lets-chat-ce40a-default-rtdb.firebaseio.com",
+      projectId: "lets-chat-ce40a",
+      storageBucket: "lets-chat-ce40a.appspot.com",
+      messagingSenderId: "79953368245",
+      appId: "1:79953368245:web:f53e49b203d348612e1afc",
+      measurementId: "G-T8YFP5NFLV"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
